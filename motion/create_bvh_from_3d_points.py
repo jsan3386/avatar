fixing hips rotation

motion/create_bvh_from_3d_points.py
@@ -11,7 +11,7 @@ import numpy as np
 def rigid_transform_3D(A, B):
 	assert len(A) == len(B)
 
-	N = A.shape[0]; # total points
+	N = A.shape[0] # total points
 
 	centroid_A = np.mean(A, axis=0)
 	centroid_B = np.mean(B, axis=0)

motion/retarget_motion_mine/transfer_motion.py
@@ -1,5 +1,6 @@
 import bpy
 from mathutils import Matrix, Vector
+import numpy as np
 
 source = bpy.data.objects["02_01_walk"]
 target = bpy.data.objects["Standard"]
@@ -8,6 +9,37 @@ file_bone_correspondences = "/home/jsanchez/Software/gitprojects/avatar/bone_cor
 #file_bone_correspondences = "/home/jsanchez/Software/gitprojects/avatar/bone_correspondance_mixamo.txt"
 #file_bone_correspondences = "/Users/jsanchez/Software/gitprojects/avatar/bone_correspondance_mixamo.txt"
 
+
+def rigid_transform_3D(A, B):
+    assert len(A) == len(B)
+
+    N = A.shape[0] # total points
+
+    centroid_A = np.mean(A, axis=0)
+    centroid_B = np.mean(B, axis=0)
+    
+    # centre the points
+    AA = A - np.tile(centroid_A, (N, 1))
+    BB = B - np.tile(centroid_B, (N, 1))
+
+    # dot is matrix multiplication for array
+    H = np.transpose(AA) * BB
+
+    U, S, Vt = np.linalg.svd(H)
+
+    R = Vt.T * U.T
+
+    # special reflection case
+    if np.linalg.det(R) < 0:
+       #print ("Reflection detected")
+       Vt[2,:] *= -1
+       R = Vt.T * U.T
+
+    t = -R*centroid_A.T + centroid_B.T
+
+    #print (t)
+
+    return R, t
 
 def read_text_lines(filename):
 
@@ -91,12 +123,25 @@ for f in range(nfirst, nlast):
                 pb.location = target.matrix_world.inverted() @ pb.bone.matrix_local.inverted() @ loc
                 pb.keyframe_insert('location', frame=f, group=pb.name)
 
-                spb.rotation_mode = 'XYZ'
+                # compute translation and first rotation between rest position and desired points
+                A = np.mat((ref_skel[14,:], ref_skel[8,:], ref_skel[11,:], ref_skel[1,:]))
+                B = np.mat((pts_skel[14,:], pts_skel[8,:], pts_skel[11,:], pts_skel[1,:]))
+                R, T = rigid_transform_3D(A,B)
+
+                mR = Matrix([[R[0,0],R[0,1],R[0,2]], [R[1,0],R[1,1],R[1,2]], [R[2,0],R[2,1],R[2,2]]])
+
+                boneRefPoseMtx = pb.bone.matrix_local.copy()
+                rotMtx = boneRefPoseMtx.inverted() * mR * boneRefPoseMtx
                 pb.rotation_mode = 'XYZ'
-                #rot = spb.rotation_euler
-                rot = (spb.matrix_basis @ matrices_target[pb.name]).to_euler()
-                pb.rotation_euler = rot
+                pb.rotation_euler = rot.to_euler()
                 pb.keyframe_insert('rotation_euler', frame=f, group=pb.name)
+
+                # spb.rotation_mode = 'XYZ'
+                # pb.rotation_mode = 'XYZ'
+                # #rot = spb.rotation_euler
+                # rot = (spb.matrix_basis @ matrices_target[pb.name]).to_euler()
+                # pb.rotation_euler = rot
+                # pb.keyframe_insert('rotation_euler', frame=f, group=pb.name)
 
 
 